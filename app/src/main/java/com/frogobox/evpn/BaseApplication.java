2b2489ddd9eedcd14d5b64601c8cd3375819bce7
amirisback <faisalamircs@gmail.com>
2019-11-26 17:51:51 +0700

    create: BaseApplication.java

--- app/src/main/java/com/frogobox/evpn/BaseApplication.java
+++ app/src/main/java/com/frogobox/evpn/BaseApplication.java
@@ -3,29 +3,27 @@ package com.frogobox.evpn;
 import android.app.Application;
 import android.content.Context;
 
-public class App extends Application {
+public class BaseApplication extends Application {
 
-    private static App instance;
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-
-        instance = this;
-    }
-
-
-    @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
-    }
+    private static BaseApplication instance;
 
     public static String getResourceString(int resId) {
         return instance.getString(resId);
     }
 
-    public static App getInstance() {
+    public static BaseApplication getInstance() {
         return instance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
     }
 
 }
