67e011c86296b7844b9db917ccc36fb94b286b58
amirisback <faisalamircs@gmail.com>
2019-11-26 17:51:26 +0700

    update: content of Constant.kt

--- app/src/main/java/com/frogobox/evpn/helper/Constant.kt
+++ app/src/main/java/com/frogobox/evpn/helper/Constant.kt
@@ -20,7 +20,21 @@ package com.frogobox.evpn.helper
 class Constant {
 
     object Variable {
+
+        const val LOAD_ERROR = 0
+        const val DOWNLOAD_PROGRESS = 1
+        const val PARSE_PROGRESS = 2
+        const val LOADING_SUCCESS = 3
+        const val SWITCH_TO_RESULT = 4
+
+        const val START_VPN_PROFILE = 70
+
+        const val BASE_URL = "http://www.vpngate.net/api/iphone/"
+        const val BASE_FILE_NAME = "vpngate.csv"
+        const val BROADCAST_ACTION = "de.blinkt.openvpn.VPN_STATUS"
+
         const val EXTRA_COUNTRY = "country";
+
     }
 
 
