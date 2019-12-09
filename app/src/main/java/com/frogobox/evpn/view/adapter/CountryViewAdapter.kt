425ca5eed80de154852f96fac0117a6048d5fd65
amirisback <faisalamircs@gmail.com>
2019-12-09 18:44:50 +0700

    update: build.gradle

---
+++ app/src/main/java/com/frogobox/evpn/view/adapter/CountryViewAdapter.kt
@@ -0,0 +1,22 @@
+package com.frogobox.evpn.view.adapter
+
+/**
+ * Created by Faisal Amir
+ * FrogoBox Inc License
+ * =========================================
+ * Frogobox-VPN-Hero
+ * Copyright (C) 02/12/2019.
+ * All rights reserved
+ * -----------------------------------------
+ * Name     : Muhammad Faisal Amir
+ * E-mail   : faisalamircs@gmail.com
+ * Github   : github.com/amirisback
+ * LinkedIn : linkedin.com/in/faisalamircs
+ * -----------------------------------------
+ * FrogoBox Software Industries
+ * com.frogobox.evpn.view.adapter
+ *
+ */
+class CountryViewAdapter {
+
+}
